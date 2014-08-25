885b102cbf02fb047f53a075f15e16641721dbf5
Leon Ehrenhart <leonehrenhart@leons-mbp.speedport_w_723v_1_32_000>
2014-08-25 14:16:28 +0200

    Initial Commit

---
+++ meldung/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  meldung
+//
+//  Created by Leon Ehrenhart on 25.08.14.
+//  Copyright (c) 2014 Leon Ehrenhart. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ meldung/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  meldung
+//
+//  Created by Leon Ehrenhart on 25.08.14.
+//  Copyright (c) 2014 Leon Ehrenhart. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
